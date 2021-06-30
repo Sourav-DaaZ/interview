cfed60a64d2f9f6b1488028dbf8b0a889e225e5c
sourav Das <sourav.das@mantralabsglobal.com>
2021-06-30 23:39:04 +0530

    fetch data from api and show in table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 import React, { useEffect, useState } from "react";
 import TableComponent from "./commonComponent/tableComponent";
+import OutsideAuthApi from "./services/outsideAuth";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [toogle, setToogle] = useState(0);
 
+  useEffect(() => {
+    OutsideAuthApi()
+      .fetchData()
+      .then((res) => {
+        let dataValue = [];
+        if (res) {
+          res.map((x, index) =>
+            dataValue.push({
+              id: index,
+              name: x.firstname + "" + x.lastname,
+              email: x.email,
+              img: x.avatarUrl,
+              premum: x.hasPremium,
+              phone: x.phone,
+              allBids: x.bids,
+              maxBid: Math.max.apply(Math, x.bids.map(function(o) { return o.amount; })),
+              minBid: Math.min.apply(Math, x.bids.map(function(o) { return o.amount; }))
+            })
+          );
+        }
+        console.log(dataValue)
+        setData(dataValue);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },[]);
+
+  // componentDidUpdate() {
+
+  // }
 
   const columns = [
     {
@@ -28,7 +62,7 @@ function App() {
     },
     {
       field: "premum",
-      headerName: "Full name",
+      headerName: "Premum",
       sortable: false,
       width: 150,
     },
@@ -36,12 +70,19 @@ function App() {
       field: "bid",
       headerName: "Max/Min Bid",
       sortable: true,
+      description: 'please cleack for show max and min bid',
       width: 250,
+      renderCell: (params) => {
+        return (
+          <div className="d-flex justify-content-between align-items-center" style={{ cursor: "pointer" }}>
+            {toogle?params.row.maxBid:params.row.minBid}
+          </div>
+        );
+      },
     },
   ];
 
-  const rows = [{ id: 1, name: "Snow", age: 35, img: "http://i.imgur.com/BCbxVui.png" }];
-  return <TableComponent rows={rows} columns={columns} loading={false} />;
+  return <TableComponent rows={data} columns={columns} loading={false} />;
 }
 
 export default App;
